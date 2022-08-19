e1d3dda0934823bc83c7ad07bb69624771c1cb45
pelumikolade <111541088+pelumikolade@users.noreply.github.com>
2022-08-19 07:08:36 -0400

    Update config.tf
    First Commit

--- aws_servers/config.tf
+++ aws_servers/config.tf
@@ -31,3 +31,14 @@ data "aws_ami" "latest_amazon_linux" {
 #  }
 #}
 
+# Data source for AMI id
+#data "aws_ami" "latest_ubuntu_linux" {
+#  owners      = ["amazon"]
+#  owners      = ["099720109477"]
+#  most_recent = true
+#  filter {
+#    name   = "name"
+#    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+#  }
+#}
+
